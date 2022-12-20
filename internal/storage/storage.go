86ae5df7ea3340ba5c8c459439d62ccab06352d4
inkpics <110731828+inkpics@users.noreply.github.com>
2022-12-20 17:22:27 +0300

    Fix 18_2

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -3,23 +3,22 @@ package storage
 import (
 	"fmt"
 
-	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
 	"github.com/lib/pq"
 )
 
 type Storage struct {
-	DatabaseAddr    string
+	sqlDB           *sqlx.DB
 	ErrDuplicateKey error
 }
 
-type User struct {
+type user struct {
 	ID       string `db:"id"`
 	Login    string `db:"login"`
 	Password string `db:"password"`
 }
 
-type Order struct {
+type order struct {
 	ID         string  `db:"id"`
 	Login      string  `db:"login"`
 	Number     string  `db:"number"`
@@ -28,14 +27,14 @@ type Order struct {
 	UploadedAt string  `db:"uploaded_at"`
 }
 
-type Balance struct {
+type balance struct {
 	ID        string  `db:"id"`
 	Login     string  `db:"login"`
 	Current   float64 `db:"current"`
 	Withdrawn float64 `db:"withdrawn"`
 }
 
-type Withdraw struct {
+type withdraw struct {
 	ID          string  `db:"id"`
 	Login       string  `db:"login"`
 	OrderNumber string  `db:"order_number"`
@@ -44,18 +43,19 @@ type Withdraw struct {
 }
 
 func New(databaseAddr string) (*Storage, error) {
+	db, err := sqlx.Connect("postgres", databaseAddr)
+	if err != nil {
+		return nil, fmt.Errorf("db connect: %w", err)
+	}
+
 	s := &Storage{
-		DatabaseAddr:    databaseAddr,
+		sqlDB:           db,
 		ErrDuplicateKey: fmt.Errorf("duplicate key"),
 	}
 
-	db, err := sqlx.Connect("postgres", s.DatabaseAddr)
-	if err != nil {
-		return s, fmt.Errorf("db connect: %w", err)
-	}
-	defer db.Close()
+	s.sqlDB.MustExec(`
+		CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-	db.MustExec(`
         CREATE TABLE IF NOT EXISTS gom_users (
             id text primary key,
             login text unique,
@@ -87,166 +87,101 @@ func New(databaseAddr string) (*Storage, error) {
         );
     `)
 
-	user := User{}
-	rows, err := db.Queryx("SELECT * FROM gom_users")
-	if err != nil {
-		return s, fmt.Errorf("read rows: %w", err)
-	}
-	defer rows.Close()
-	for rows.Next() {
-		err := rows.StructScan(&user)
-		if err != nil {
-			return s, fmt.Errorf("rows struct scan: %w", err)
-		}
-	}
-	err = rows.Err()
-	if err != nil {
-		return s, fmt.Errorf("rows error: %w", err)
-	}
-
 	return s, nil
 }
 
-func uuidV4() string {
-	id := uuid.New()
-	return id.String()
-}
-
 func (s *Storage) UserRegister(login, password string) error {
-	db, err := sqlx.Connect("postgres", s.DatabaseAddr)
+	tx, err := s.sqlDB.Begin()
 	if err != nil {
-		return fmt.Errorf("sql connect: %w", err)
+		return fmt.Errorf("tx error: %w", err)
 	}
-	defer db.Close()
 
-	_, err = db.Exec("INSERT INTO gom_users VALUES ($1, $2, $3)", uuidV4(), login, password)
+	_, err = tx.Exec("INSERT INTO gom_users VALUES (gen_random_uuid(), $1, $2)", login, password)
 	if err != nil {
 		if err, ok := err.(*pq.Error); ok {
 			if err.Code == "23505" {
 				return s.ErrDuplicateKey
 			}
 		}
-	}
-
-	_, err = db.Exec("INSERT INTO gom_balances VALUES ($1, $2, 0, 0)", uuidV4(), login)
-	if err != nil {
-		if err, ok := err.(*pq.Error); ok {
-			if err.Code == "23505" {
-				return s.ErrDuplicateKey
-			}
-		}
-	}
-
-	if err != nil {
 		return fmt.Errorf("db error: %w", err)
 	}
+
+	_, err = tx.Exec("INSERT INTO gom_balances VALUES (gen_random_uuid(), $1, 0, 0)", login)
+	if err != nil {
+		if err, ok := err.(*pq.Error); ok {
+			if err.Code == "23505" {
+				return s.ErrDuplicateKey
+			}
+		}
+		return fmt.Errorf("db error: %w", err)
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return fmt.Errorf("commit error: %w", err)
+	}
+
 	return nil
 }
 
 func (s *Storage) UserLogin(login, password string) error {
-	db, err := sqlx.Connect("postgres", s.DatabaseAddr)
-	if err != nil {
-		return fmt.Errorf("sql connect: %w", err)
-	}
-	defer db.Close()
-
 	var count int
-	rows, err := db.Queryx("SELECT COUNT(*) FROM gom_users WHERE login = $1 AND password = $2", login, password)
+	err := s.sqlDB.QueryRowx("SELECT COUNT(*) FROM gom_users WHERE login = $1 AND password = $2", login, password).Scan(&count)
 	if err != nil {
 		return fmt.Errorf("read rows: %w", err)
 	}
-	defer rows.Close()
-	for rows.Next() {
-		if err := rows.Scan(&count); err != nil {
-			return fmt.Errorf("read rows: %w", err)
-		}
-		if count < 1 {
-			return fmt.Errorf("no credentials matches: %w", err)
-		}
-	}
-	err = rows.Err()
-	if err != nil {
-		return fmt.Errorf("rows error: %w", err)
+	if count < 1 {
+		return fmt.Errorf("no credentials matches: %w", err)
 	}
 
 	return nil
 }
 
 func (s *Storage) OrderRegistered(login, orderNumber string) (int, error) {
-	db, err := sqlx.Connect("postgres", s.DatabaseAddr)
-	if err != nil {
-		return 0, fmt.Errorf("sql connect: %w", err)
-	}
-	defer db.Close()
-
-	order := Order{}
-	rows, err := db.Queryx("SELECT * FROM gom_orders WHERE number = $1", orderNumber)
+	o := order{}
+	err := s.sqlDB.QueryRowx("SELECT * FROM gom_orders WHERE number = $1 LIMIT 1", orderNumber).StructScan(&o)
 	if err != nil {
 		return 0, fmt.Errorf("read rows: %w", err)
 	}
-	defer rows.Close()
-	for rows.Next() {
-		if err := rows.StructScan(&order); err != nil {
-			return 0, fmt.Errorf("read rows: %w", err)
-		}
-		if order.Login == login {
-			return 1, nil
-		} else if order.Login != "" {
-			return -1, nil
-		}
-	}
-	err = rows.Err()
-	if err != nil {
-		return 0, fmt.Errorf("rows error: %w", err)
+	if o.Login == login {
+		return 1, nil
+	} else if o.Login != "" {
+		return -1, nil
 	}
 
 	return 0, nil
 }
 
 func (s *Storage) OrderRegister(login, orderNumber string) error {
-	db, err := sqlx.Connect("postgres", s.DatabaseAddr)
-	if err != nil {
-		return fmt.Errorf("sql connect: %w", err)
-	}
-	defer db.Close()
-
-	_, err = db.Exec("INSERT INTO gom_orders VALUES ($1, $2, $3, 'NEW', 0, NOW())", uuidV4(), login, orderNumber)
+	_, err := s.sqlDB.Exec("INSERT INTO gom_orders VALUES (gen_random_uuid(), $1, $2, 'NEW', 0, NOW())", login, orderNumber)
 	if err != nil {
 		if err, ok := err.(*pq.Error); ok {
 			if err.Code == "23505" {
 				return s.ErrDuplicateKey
 			}
 		}
-	}
-
-	if err != nil {
 		return fmt.Errorf("db error: %w", err)
 	}
+
 	return nil
 }
 
-func (s *Storage) Orders(login string) ([]Order, error) {
-	var result []Order
+func (s *Storage) Orders(login string) ([]order, error) {
+	var result []order
 
-	db, err := sqlx.Connect("postgres", s.DatabaseAddr)
-	if err != nil {
-		return result, fmt.Errorf("sql connect: %w", err)
-	}
-	defer db.Close()
-
-	order := Order{}
-	rows, err := db.Queryx("SELECT * FROM gom_orders WHERE login =$1", login)
+	o := order{}
+	rows, err := s.sqlDB.Queryx("SELECT * FROM gom_orders WHERE login =$1", login)
 	if err != nil {
 		return result, fmt.Errorf("read rows: %w", err)
 	}
 	defer rows.Close()
 
 	for rows.Next() {
-		err := rows.StructScan(&order)
+		err := rows.StructScan(&o)
 		if err != nil {
 			return result, fmt.Errorf("rows struct scan: %w", err)
 		}
-		result = append(result, order)
+		result = append(result, o)
 	}
 
 	err = rows.Err()
@@ -257,96 +192,66 @@ func (s *Storage) Orders(login string) ([]Order, error) {
 	return result, nil
 }
 
-func (s *Storage) UserBalance(login string) (Balance, error) {
-	balance := Balance{}
+func (s *Storage) UserBalance(login string) (balance, error) {
+	b := balance{}
 
-	db, err := sqlx.Connect("postgres", s.DatabaseAddr)
+	err := s.sqlDB.QueryRowx("SELECT * FROM gom_balances WHERE login = $1 LIMIT 1", login).StructScan(&b)
 	if err != nil {
-		return balance, fmt.Errorf("sql connect: %w", err)
-	}
-	defer db.Close()
-
-	rows, err := db.Queryx("SELECT * FROM gom_balances WHERE login = $1", login)
-	if err != nil {
-		return balance, fmt.Errorf("read rows: %w", err)
-	}
-	defer rows.Close()
-
-	rows.Next()
-	err = rows.StructScan(&balance)
-	if err != nil {
-		return balance, fmt.Errorf("rows struct scan: %w", err)
+		return b, fmt.Errorf("read rows: %w", err)
 	}
 
-	err = rows.Err()
-	if err != nil {
-		return balance, fmt.Errorf("rows error: %w", err)
-	}
-
-	return balance, nil
+	return b, nil
 }
 
 func (s *Storage) Withdraw(login, orderNumber string, sum float64) (int, error) {
-	db, err := sqlx.Connect("postgres", s.DatabaseAddr)
-	if err != nil {
-		return 0, fmt.Errorf("sql connect: %w", err)
-	}
-	defer db.Close()
-
-	balance := Balance{}
-	rows, err := db.Queryx("SELECT * FROM gom_balances WHERE login = $1", login)
+	b := balance{}
+	err := s.sqlDB.QueryRowx("SELECT * FROM gom_balances WHERE login = $1 LIMIT 1", login).StructScan(&b)
 	if err != nil {
 		return 0, fmt.Errorf("read rows: %w", err)
 	}
-	defer rows.Close()
-	for rows.Next() {
-		if err := rows.StructScan(&balance); err != nil {
-			return 0, fmt.Errorf("read rows: %w", err)
-		}
-		if balance.Current < sum {
-			return 402, nil
-		}
-	}
-	err = rows.Err()
-	if err != nil {
-		return 0, fmt.Errorf("rows error: %w", err)
+	if b.Current < sum {
+		return 402, nil
 	}
 
-	_, err = db.Exec("UPDATE gom_balances SET current = $1, withdrawn = $2 WHERE login = $3", balance.Current-sum, balance.Withdrawn+sum, login)
+	tx, err := s.sqlDB.Begin()
+	if err != nil {
+		return 0, fmt.Errorf("tx error: %w", err)
+	}
+
+	_, err = tx.Exec("UPDATE gom_balances SET current = $1, withdrawn = $2 WHERE login = $3", b.Current-sum, b.Withdrawn+sum, login)
 	if err != nil {
 		return 0, fmt.Errorf("db error: %w", err)
 	}
 
-	_, err = db.Exec("INSERT INTO gom_withdrawals VALUES ($1, $2, $3, $4, NOW())", uuidV4(), login, orderNumber, sum)
+	_, err = tx.Exec("INSERT INTO gom_withdrawals VALUES (gen_random_uuid(), $1, $2, $3, NOW())", login, orderNumber, sum)
 	if err != nil {
 		return 0, fmt.Errorf("db error: %w", err)
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return 0, fmt.Errorf("commit error: %w", err)
 	}
 
 	return 0, nil
 }
 
-func (s *Storage) Withdrawals(login string) ([]Withdraw, error) {
-	var result []Withdraw
+func (s *Storage) Withdrawals(login string) ([]withdraw, error) {
+	var result []withdraw
 
-	db, err := sqlx.Connect("postgres", s.DatabaseAddr)
-	if err != nil {
-		return result, fmt.Errorf("sql connect: %w", err)
-	}
-	defer db.Close()
-
-	withdraw := Withdraw{}
-	rows, err := db.Queryx("SELECT * FROM gom_withdrawals")
+	wd := withdraw{}
+	rows, err := s.sqlDB.Queryx("SELECT * FROM gom_withdrawals")
 	if err != nil {
 		return result, fmt.Errorf("read rows: %w", err)
 	}
 	defer rows.Close()
 
 	for rows.Next() {
-		err := rows.StructScan(&withdraw)
+		err := rows.StructScan(&wd)
 		if err != nil {
 			return result, fmt.Errorf("rows struct scan: %w", err)
 		}
-		result = append(result, withdraw)
+		result = append(result, wd)
 	}
 
 	err = rows.Err()
@@ -357,33 +262,27 @@ func (s *Storage) Withdrawals(login string) ([]Withdraw, error) {
 	return result, nil
 }
 
-func (s *Storage) OrdersProcessing() ([]Order, error) {
-	var result []Order
+func (s *Storage) OrdersProcessing() ([]order, error) {
+	var result []order
 
-	db, err := sqlx.Connect("postgres", s.DatabaseAddr)
-	if err != nil {
-		return result, fmt.Errorf("sql connect: %w", err)
-	}
-	defer db.Close()
-
-	_, err = db.Exec("UPDATE gom_orders SET status = 'PROCESSING' WHERE status = 'NEW'")
+	_, err := s.sqlDB.Exec("UPDATE gom_orders SET status = 'PROCESSING' WHERE status = 'NEW'")
 	if err != nil {
 		return result, fmt.Errorf("db update error: %w", err)
 	}
 
-	order := Order{}
-	rows, err := db.Queryx("SELECT * FROM gom_orders WHERE status = 'PROCESSING'")
+	o := order{}
+	rows, err := s.sqlDB.Queryx("SELECT * FROM gom_orders WHERE status = 'PROCESSING'")
 	if err != nil {
 		return result, fmt.Errorf("read rows: %w", err)
 	}
 	defer rows.Close()
 
 	for rows.Next() {
-		err := rows.StructScan(&order)
+		err := rows.StructScan(&o)
 		if err != nil {
 			return result, fmt.Errorf("rows struct scan: %w", err)
 		}
-		result = append(result, order)
+		result = append(result, o)
 	}
 
 	err = rows.Err()
@@ -395,13 +294,7 @@ func (s *Storage) OrdersProcessing() ([]Order, error) {
 }
 
 func (s *Storage) SetOrderInvalid(orderNumber string) error {
-	db, err := sqlx.Connect("postgres", s.DatabaseAddr)
-	if err != nil {
-		return fmt.Errorf("sql connect: %w", err)
-	}
-	defer db.Close()
-
-	_, err = db.Exec("UPDATE gom_orders SET status = 'INVALID' WHERE number = $1", orderNumber)
+	_, err := s.sqlDB.Exec("UPDATE gom_orders SET status = 'INVALID' WHERE number = $1", orderNumber)
 	if err != nil {
 		return fmt.Errorf("db update error: %w", err)
 	}
@@ -410,67 +303,45 @@ func (s *Storage) SetOrderInvalid(orderNumber string) error {
 }
 
 func (s *Storage) UserFromOrderNumber(orderNumber string) (string, error) {
-	db, err := sqlx.Connect("postgres", s.DatabaseAddr)
-	if err != nil {
-		return "", fmt.Errorf("sql connect: %w", err)
-	}
-	defer db.Close()
-
 	var user string
-	rows, err := db.Queryx("SELECT login FROM gom_orders WHERE number = $1", orderNumber)
+	err := s.sqlDB.QueryRowx("SELECT login FROM gom_orders WHERE number = $1 LIMIT 1", orderNumber).Scan(&user)
 	if err != nil {
 		return "", fmt.Errorf("read rows: %w", err)
 	}
-	defer rows.Close()
 
-	rows.Next()
-	if err := rows.Scan(&user); err != nil {
-		return "", fmt.Errorf("read rows: %w", err)
-	}
-
-	err = rows.Err()
-	if err != nil {
-		return "", fmt.Errorf("rows error: %w", err)
-	}
 	return user, nil
 }
 
 func (s *Storage) SetOrderProcessed(orderNumber string, accrual float64) error {
-	db, err := sqlx.Connect("postgres", s.DatabaseAddr)
-	if err != nil {
-		return fmt.Errorf("sql connect: %w", err)
-	}
-	defer db.Close()
-
-	_, err = db.Exec("UPDATE gom_orders SET status = 'PROCESSED', accrual = $1 WHERE number = $2", accrual, orderNumber)
-	if err != nil {
-		return fmt.Errorf("db update error: %w", err)
-	}
-
 	login, err := s.UserFromOrderNumber(orderNumber)
 	if err != nil {
 		return fmt.Errorf("balance update user error: %w", err)
 	}
 
-	balance := Balance{}
-	rows, err := db.Queryx("SELECT * FROM gom_balances WHERE login = $1", login)
+	b := balance{}
+	err = s.sqlDB.QueryRowx("SELECT * FROM gom_balances WHERE login = $1 LIMIT 1", login).StructScan(&b)
 	if err != nil {
 		return fmt.Errorf("read rows: %w", err)
 	}
-	defer rows.Close()
-	for rows.Next() {
-		if err := rows.StructScan(&balance); err != nil {
-			return fmt.Errorf("read rows: %w", err)
-		}
-	}
-	err = rows.Err()
+
+	tx, err := s.sqlDB.Begin()
 	if err != nil {
-		return fmt.Errorf("rows error: %w", err)
+		return fmt.Errorf("tx error: %w", err)
 	}
 
-	_, err = db.Exec("UPDATE gom_balances SET current = $1 WHERE login = $2", balance.Current+accrual, login)
+	_, err = tx.Exec("UPDATE gom_orders SET status = 'PROCESSED', accrual = $1 WHERE number = $2", accrual, orderNumber)
+	if err != nil {
+		return fmt.Errorf("db update error: %w", err)
+	}
+
+	_, err = tx.Exec("UPDATE gom_balances SET current = $1 WHERE login = $2", b.Current+accrual, login)
 	if err != nil {
 		return fmt.Errorf("db error: %w", err)
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return fmt.Errorf("commit error: %w", err)
 	}
 
 	return nil
